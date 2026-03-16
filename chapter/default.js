4c29c8693e4cdaec57be05028214de0248111190
icpc87 <mail@ic2.jp>
2026-03-16 13:18:25 +0900

    Update default.js

--- chapter/default.js
+++ chapter/default.js
@@ -1,1 +1,7 @@
-
+const defaultInfo = {
+  episodeNumber: "307",
+  youtubeLink: "https://youtu.be/GN2QfR15wwA",
+  youtubeStart: "2:19:35",
+  rumbleLink: "https://rumble.com/v6ylc3q",
+  rumbleStart: "2:20:22"
+};
