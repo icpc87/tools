32b6ab67214dd3dd4170881e4cd3f5f7aa5a7426
icpc87 <mail@ic2.jp>
2026-04-27 21:26:54 +0900

    Update default.js

--- chapter/default.js
+++ chapter/default.js
@@ -1,8 +1,8 @@
 const defaultInfo = {
-  episodeNumber: "310",
-  publishDate: "2026-03-15",
-  youtubeLink: "https://youtu.be/-XW-nnls_Vc",
-  youtubeStart: "2:19:35",
-  rumbleLink: "https://rumble.com/v774kmu-newepisode310heavenese-style-2026.3.15.html",
-  rumbleStart: "2:36:03"
+  episodeNumber: "316",
+  publishDate: "2026-04-26",
+  youtubeLink: "https://youtu.be/YyfhBJMbxmc",
+  youtubeStart: "2:23:16",
+  rumbleLink: "https://rumble.com/v790516-newepisode3161954-heavenese-style-2026.4.19.html",
+  rumbleStart: "2:24:57"
 };
