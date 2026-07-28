635f2231ce5924617ccd93a28cb48058b44c9232
icpc87 <mail@ic2.jp>
2026-07-28 20:16:46 +0900

    Update default.js

--- chapter/default.js
+++ chapter/default.js
@@ -1,8 +1,8 @@
 const defaultInfo = {
-  episodeNumber: "328",
-  publishDate: "2026-07-19",
-  youtubeLink: "https://youtu.be/9gCa8FMZkSw",
-  youtubeStart: "2:09:47",
+  episodeNumber: "329",
+  publishDate: "2026-07-26",
+  youtubeLink: "https://youtu.be/6j2kVkUDIZ0",
+  youtubeStart: "1:53:01",
   rumbleLink: "https://rumble.com/v7cxnbu-newepisode328heavenese-style-2026.7.19.html",
-  rumbleStart: "2:10:19"
+  rumbleStart: "1:53:45"
 };
