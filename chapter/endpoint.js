7dc2f95ebbda8b04720e014fe3071be6ae22b8e0
icpc87 <mail@ic2.jp>
2026-09-02 00:23:31 +0900

    Update endpoint.js

--- chapter/endpoint.js
+++ chapter/endpoint.js
@@ -1,5 +1,11 @@
 {
-  "episodeApi": "https://script.google.com/macros/s/AKfycbwv40oZrjKzRuC6zi7UA0CyTawWJAhkHNJAcaQjXc6yTU6xhahcNOxDuWGw-_wCgbir/exec",
-  "chapterApi": "https://script.google.com/macros/s/AKfycbxEA_n2U_EmDm-cPGN_DKYa5dtVT1YG6MtbBz6b6gkr1hg99LWQyIdxMm4sJZZ9vIX5/exec",
-  "storeApi": "https://script.google.com/macros/s/AKfycbyyzEdqEVuKzj9cffMmZvcJOX_Gn1cPOey2hrQivn2BV9sRKdX3XuwqA_EXkWKRID1B/exec"
+  "episodeApi": "https://script.google.com/macros/s/AKfycbwv40oZrjKzRuC6zi7UA0CyTawWJAhkHNJAcaQjXc6yTU6xhahcNOxDuWGw-_wCgbir/exec?sheet=programs",
+  "chapterApi": "https://script.google.com/macros/s/AKfycbwv40oZrjKzRuC6zi7UA0CyTawWJAhkHNJAcaQjXc6yTU6xhahcNOxDuWGw-_wCgbir/exec?sheet=chapters",
+  "storeApi": "https://script.google.com/macros/s/AKfycbyyzEdqEVuKzj9cffMmZvcJOX_Gn1cPOey2hrQivn2BV9sRKdX3XuwqA_EXkWKRID1B/exec",
+
+  "combinedApi": "https://script.google.com/macros/s/AKfycbwLtdALYvN5TsQtRFRM2KDgGLVCXS3KNrtb-rfQs9c1wUxUgXm0rSZJpsY-MjlI4wv6/exec",
+  "metaApi": "https://script.google.com/macros/s/AKfycbw8bbOvM1l_535IZAc00MN4cpS32xup4SCt5DFVjyY7p2EQb0vP6w_9q9syCx0vk84liA/exec",
+  "ytMainApi": "https://script.google.com/macros/s/AKfycbysN4tlaX9ufii1toBKkdh7cERzMHmMDoEhma7jaOutKClPGxTPwHbkqUHGB0V8mFzgng/exec",
+  "ytSubApi": "https://script.google.com/macros/s/AKfycbx0rc6v4E0y5h1UfgWd6iFMdbqa11HGLNB_y6ITuZ5CFV1_y3EvlL2gOW-Hjlhk2K_u7A/exec",
+  "ytSub2Api": "https://script.google.com/macros/s/AKfycbyU6U4mfzv1UWEC-MOFf9s88Za5zST_HIPw59Jzxw7ekPbO_yFopeoa82H2Zbi3oZqCmA/exec"
 }
